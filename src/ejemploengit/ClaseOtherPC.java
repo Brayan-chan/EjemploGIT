55e5768bef0eeaa8d059db43d04123258b002285
B-Enterprise <152478482+B-Enterprise@users.noreply.github.com>
2024-03-02 16:15:33 -0600

    Subiendo archivo desde otra pc

---
+++ src/ejemploengit/ClaseOtherPC.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemploengit;
+
+/**
+ *
+ * @author Brayan Chan
+ */
+public class ClaseOtherPC {
+    public static void main(String[] args) {
+        System.out.println("Hola desde la otra pc");
+    }
+}
